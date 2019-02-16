7b4677076a379bc6d4c74229e823eae7c6035942
Corentin Poupry <corentin.poupry@protonmail.com>
2019-02-16 20:19:11 +0100

    add examples

---
+++ examples/congruential.py
@@ -0,0 +1,46 @@
+"""
+tpe-2019 :: Générer l'aléatoire
+
+Exemple d'un générateur congruentiel linéaire en Python
+
+Corentin POUPRY <HelloEdit>
+Mika LIAO <Datacre>
+"""
+
+
+def gcm(m, a, c, x):
+    """
+        Générateur congruentiel linéaire en utilisant la formule
+        telle que décrite par Derrick Lehmer ainsi que les générateurs
+        Python (https://www.programiz.com/python-programming/generator)
+    """
+    while True:
+        yield x
+        x = (a * x + c) % m
+
+
+seed = 125
+# seed = 96
+# seed = 50
+# seed = 10
+
+
+gen = gcm(a=25, c=16, m=256, x=seed)
+
+result = []
+for i in range(500):
+    result.append(next(gen))
+
+unique = set(result)
+
+
+print("\n")
+
+print("- {} résultats générés par le générateur".format(len(result)))
+print(result, end="\n\n")
+
+print("- {} résultats uniques générés".format(len(unique)))
+print(unique, end="\n\n")
+
+print("- Motif récurrent")
+print(result[:len(unique)])
