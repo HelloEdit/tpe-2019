60297674e02194e319ae44b024c27fc6f9e8b318
Corentin Poupry <corentin.poupry@protonmail.com>
2019-03-28 22:42:34 +0100

    update

--- theme.js
+++ theme.js
@@ -1,9 +1,11 @@
 import { dark } from 'mdx-deck/themes'
 import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
+import python from 'react-syntax-highlighter/languages/prism/python'
 
 export default {
   ...dark,
   prism: {
-    style: okaidia
+    style: okaidia,
+    languages: { python }
   }
 }
